645d611a6e96f76e2ca44c1909706412e3bfd499
lobotomised <romain@rprevost.fr>
2022-02-08 14:20:32 +0100

    can read db connexion info from .env

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,13 +3,31 @@
 namespace Lobotomised\Dbbackup\Test;
 
 use Lobotomised\Dbbackup\ServiceProvider;
+use Illuminate\Foundation\Bootstrap\LoadEnvironmentVariables;
 
-class TestCase extends \Orchestra\Testbench\TestCase
+abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
     protected function getPackageProviders($app)
     {
         return [
             ServiceProvider::class
         ];
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        $app->useEnvironmentPath(__DIR__.'/..');
+        $app->bootstrapWith([LoadEnvironmentVariables::class]);
+        parent::getEnvironmentSetUp($app);
+
+        $app['config']->set('database.connections', env('DB_CONNECTION', 'mysql'));
+        $app['config']->set('database.connections.mysql', [
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+        ]);
+
     }
 }
