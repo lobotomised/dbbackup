76e3377eee864c15203158138a30f7c08582f125
lobotomised <34192441+lobotomised@users.noreply.github.com>
2025-07-28 10:44:28 +0000

    Fix styling

--- tests/BackupTest.php
+++ tests/BackupTest.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Storage;
 
 class BackupTest extends TestCase
 {
-    public function setUp(): void
+    protected function setUp(): void
     {
         parent::setUp();
 
